bf30c3b1a775717d3dc38642e428a48680191d52
Roland Ayala <roland.ayala@gmail.com>
2024-02-18 02:07:06 -0800

    Initial commit
    Baseline configuration

---
+++ lua/config/init.lua
@@ -0,0 +1,54 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- local plugins = "plugins"
+
+local plugins = "plugins"
+
+local opts = {
+	defaults = {
+		lazy = true,
+	},
+	install = {
+		colorscheme = { "catppuccin" },
+	},
+	rtp = {
+		disabled_plugins = {
+			"gzip",
+			"matchit",
+			"matchparen",
+			"netrw",
+			"netrwPlugin",
+			"tarPlugin",
+			"tohtml",
+			"tutor",
+			"zipPlugin",
+		},
+	},
+	change_detection = {
+		notify = false,
+	},
+}
+
+require("lazy").setup(plugins)
+
+
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,29 @@
+return {
+	{
+    "rose-pine/neovim",
+    name = "rose-pine-theme",
+    enabled = false,
+    priority = 1000,
+    lazy = false,
+    config = function()
+	require("rose-pine").setup({
+	variant = "main"
+    })
+    vim.cmd("colorscheme rose-pine")
+
+    end,
+    },
+    {
+	"catppuccin/nvim",
+	name = "catppuccin-theme",
+	priority = 1000,
+	config = function()
+	    require("catppuccin").setup({
+		-- setting props: https://github.com/catppuccin/nvim
+		flavour = "mocha",
+		transparent_background = true,
+	    })
+	    vim.cmd.colorscheme "catppuccin"
+	end
+    }
+}
